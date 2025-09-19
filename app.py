518b7f45c563818d7c81f071ad54065b303fe0d4
seocollabsoftech <seo.collabsoftech@gmail.com>
2025-09-19 14:40:01 +0530

    Another color code changed

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ st.set_page_config(
 # Custom CSS for a beautiful look
 st.markdown("""
     <style>
-    .stApp { background: linear-gradient(135deg, #f7b42c 0%, #fc575e 100%); }
+    .stApp { background: linear-gradient(135deg, #1fa2ff 0%, #12d8fa 100%); }
     .stTextInput>div>div>input {
         background-color: #fff;
         border-radius: 10px;
